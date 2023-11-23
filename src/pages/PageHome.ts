47f3a4bf92e4ed694f6a7a95b4391124b53d71fe
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-24 00:23:02 +0100

    home finished

--- src/pages/PageHome.ts
+++ src/pages/PageHome.ts
@@ -1,7 +1,15 @@
 export const PageHome = () => {
 	return /*html*/`
-<div class="page pageHome">
-	<p>the home page</p>
-</div>
+<section class="page pageHome"> 
+	<section class="bg-yellow-400 mb-3 h-44 rounded-lg text-black flex justify-center items-center text-4xl">video</section>
+	<section class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-3">
+		<div>info</div>
+		<div>info</div>
+		<div>info</div>
+		<div>info</div>
+		<div>info</div>
+		<div>info</div>
+	</section>
+</section>
 `;
 }
